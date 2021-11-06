ac33d91b6da790508f94175a305f001e5bfca844
explodingblu <190255S@mymail.nyp.edu.sg>
2021-11-06 09:24:44 +0800

    Start Change

--- BIPJ-Grp2-Team5/Main.Master.cs
+++ BIPJ-Grp2-Team5/Main.Master.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace BIPJ_Grp2_Team5
 {
-    public partial class Site_Mobile : System.Web.UI.MasterPage
+    public partial class Main : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
