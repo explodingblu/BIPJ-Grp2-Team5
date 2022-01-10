16eab22929b50e19acf5b9381464337756911c60
Nathan Kong <190471J@mymail.nyp.edu.sg>
2022-01-10 09:44:29 +0800

    nathan 10/1/2022 9.44am

---
+++ BIPJ-Grp2-Team5/Customer_ProductView.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BIPJ_Grp2_Team5
+{
+    public partial class Customer_ProductView : System.Web.UI.Page
+    {
+        Product prod = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Product aProd = new Product();
+                string prodID = Request.QueryString["Product_ID"].ToString();
+                prod = aProd.getProduct(prodID);
+                lbl_prodName.Text = prod.Product_Name;
+                lbl_prodDesc.Text = prod.Product_Desc;
+                lbl_prodPrice.Text = prod.Product_Price.ToString("c");
+                lbl_Discount.Text = prod.Discount.ToString("c");
+                img_prodImg.ImageUrl = "~\\images\\" + prod.Product_Image;
+
+                lbl_prodID.Text = prodID.ToString();
+            }
+        }
+
+        protected void Btn_Back_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ProductCatalogue.aspx");
+        }
+
+        protected void Btn_Edit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_Buy_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
