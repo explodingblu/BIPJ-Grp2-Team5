ed869d73f02e63c2e9628c88ffd0d5e1df40f81f
TAY WEI JIE, JUSTIN <194935K@mymail.nyp.edu.sg>
2022-01-17 00:22:24 +0800

    Major changes to Orders/Checkout.cs. Adjusted bootstraps for admin index and admin navigation - Justin

--- BIPJ-Grp2-Team5/Admin_Orders.aspx
+++ BIPJ-Grp2-Team5/Admin_Orders.aspx
@@ -1,5 +1,107 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin_Navbar.Master" AutoEventWireup="true" CodeBehind="Admin_Orders.aspx.cs" Inherits="BIPJ_Grp2_Team5.Admin_Orders" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    	<style>
+       
+        .auto-style2 {
+            position: relative;
+            min-height: 1px;
+            float: left;
+            width: 50%;
+            left: 1px;
+            top: 0px;
+            padding-left: 15px;
+            padding-right: 15px;
+        }
+       
+    </style>
+	<script>
+
+    </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="main" style="left: 1px; top: 0px">
+			<!-- MAIN CONTENT -->
+			<div class="main-content">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-6">
+							<!-- INPUTS -->
+							<div class="panel">
+								<div class="panel-heading">
+                                    <h3 class="panel-title">Update Order Form</h3>
+                                </div>
+
+								<div class="panel-body">
+
+									<p>Order ID: <asp:Label ID="lbl_orderid" runat="server"></asp:Label> </p>
+									<p>First Name: <asp:Label ID="lbl_fname" runat="server"></asp:Label></p>
+									<p>Last Name: <asp:Label ID="lbl_lname" runat="server"></asp:Label></p>
+									<p>Country: <asp:Label ID="lbl_country" runat="server"></asp:Label></p>
+									<p>City: <asp:Label ID="lbl_city" runat="server"></asp:Label></p>
+									<p>Address: <asp:Label ID="lbl_address" runat="server"></asp:Label></p>
+									<p>Zipcode: <asp:Label ID="lbl_zipcode" runat="server"></asp:Label></p>
+									<p>Handphone Number: <asp:Label ID="lbl_phoneno" runat="server"></asp:Label></p>
+									<p>Comments: </p><p><asp:Label ID="lbl_comment" runat="server"></asp:Label></p>
+									<p>Products: </p><p><asp:Label ID="lbl_products" runat="server"></asp:Label></p>
+									<p>Total Amount: <asp:Label ID="lbl_totalamt" runat="server"></asp:Label></p>									
+									<p>Payment Date: <asp:Label ID="lbl_date" runat="server"></asp:Label></p>
+									<p>Delivery Status: <asp:Label ID="lbl_Status" runat="server"></asp:Label></p>
+									<asp:BoundField DataField="DeliveryStatus" HeaderText="Delivery Status"/>
+									<asp:RadioButtonList ID="rbl_status" runat="server">
+                                        <asp:ListItem>Undelivered</asp:ListItem>
+                                        <asp:ListItem>Delivered</asp:ListItem>
+										<asp:ListItem>In Transit</asp:ListItem>
+										<asp:ListItem>Awaiting Design</asp:ListItem>
+                                    </asp:RadioButtonList>
+                                    <br />
+                                    <asp:Button ID="btn_submitUp" runat="server"  Text="Submit" OnClick="btn_submitUp_Click" />
+								</div>
+							</div>
+							<!-- END INPUTS -->
+						
+						</div>
+
+						</div>
+								<!-- BORDERED TABLE -->
+							<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Orders Table</h3>
+								</div>
+								<div class="panel-body">
+									<asp:GridView ID="Gv_Orders" runat="server" AutoGenerateColumns="False" DataKeyNames="Order_ID" class="table table-bordered" OnRowDeleting="Gv_Orders_RowDeleting" OnSelectedIndexChanged="Gv_Orders_SelectedIndexChanged" BackColor="#CCFFFF" BorderColor="#0066FF">
+                                        <Columns>
+                                            <asp:BoundField HeaderText="OrderID" DataField="Order_ID" />
+                                            <asp:BoundField HeaderText="FirstName" DataField="FName" />
+                                            <asp:BoundField HeaderText="LastName" DataField="LName" />
+                                            <asp:BoundField HeaderText="Country" DataField="Country" />
+                                            <asp:BoundField HeaderText="City"  DataField="City"/>
+                                            <asp:BoundField HeaderText="Address"  DataField="Address"/>
+                                            <asp:BoundField HeaderText="Zipcode" DataField="Zipcode"/>
+                                            <asp:BoundField HeaderText="PhoneNo"  DataField="PhoneNo"/>
+                                            <asp:BoundField HeaderText="Comment" DataField="Comment"/>
+                                            <asp:BoundField DataField="Product" HeaderText="Products" />
+                                            <asp:BoundField HeaderText="Total Amount" DataField="TotalAmount"/>
+											<asp:BoundField HeaderText="Payment Date" DataField="PaymentDate"/>
+											<asp:BoundField DataField="DeliveryStatus" HeaderText="Delivery Status" />
+											<asp:TemplateField ShowHeader="False">
+                                                <ItemTemplate>
+													<asp:Button ID="Button1" runat="server" CausesValidation="False" CommandName="Select" Text="Select" />
+                                                    <asp:Button ID="Button2" runat="server" CausesValidation="False" CommandName="Delete" Text="Delete" OnClientClick="return confirm('Are you sure you want to delete?');"/>
+                                                </ItemTemplate>
+                                            </asp:TemplateField>
+                                        </Columns>
+                                    </asp:GridView>
+								</div>
+							</div>
+							<!-- END BORDERED TABLE -->
+
+					</div>
+				</div>
+			</div>
+			<!-- END MAIN CONTENT -->
+            <%--</div>--%>
+		<!-- END MAIN -->
+
+
 </asp:Content>
+
--- BIPJ-Grp2-Team5/Admin_Orders.aspx.cs
+++ BIPJ-Grp2-Team5/Admin_Orders.aspx.cs
@@ -9,9 +9,174 @@ namespace BIPJ_Grp2_Team5
 {
     public partial class Admin_Orders : System.Web.UI.Page
     {
+        Checkout aCheckout = new Checkout();
+        Checkout order = null;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                bind();
+            }
+        }
+
+        protected void bind()
+        {
+            List<Checkout> checkoutList = new List<Checkout>();
+            checkoutList = aCheckout.getCheckoutAll();
+            Gv_Orders.DataSource = checkoutList;
+            Gv_Orders.DataBind();
+        }
+
+        protected void Gv_Orders_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int result = 0;
+            Checkout order = new Checkout();
+            string orderID = Gv_Orders.DataKeys[e.RowIndex].Value.ToString();
+            GridViewRow row = (GridViewRow)Gv_Orders.Rows[e.RowIndex];
+            string deliverystatus = ((TextBox)row.Cells[11].Controls[0]).Text;
+            result = order.checkoutDelete(orderID);
+
+            if (result > 0 && deliverystatus =="delivered") // admin can only remove the delivered ones
+            {
+                Response.Write("<script>alert('Order Remove successfully');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Order Removal NOT successfully');</script>");
+            }
+            bind();
 
         }
+
+        protected void Gv_Orders_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            Gv_Orders.EditIndex = e.NewEditIndex;
+            bind();
+        }
+
+        protected void Gv_Orders_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            Gv_Orders.EditIndex = -1;
+            bind();
+        }
+
+        protected void Gv_Orders_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int result = 0;
+            Checkout order = new Checkout();
+            GridViewRow row = (GridViewRow)Gv_Orders.Rows[e.RowIndex];
+            string order_ID = ((TextBox)row.Cells[0].Controls[0]).Text;
+            string fname = ((TextBox)row.Cells[1].Controls[0]).Text;
+            string lname = ((TextBox)row.Cells[2].Controls[0]).Text;
+            string country = ((TextBox)row.Cells[3].Controls[0]).Text;
+            string city = ((TextBox)row.Cells[4].Controls[0]).Text;
+            string address = ((TextBox)row.Cells[5].Controls[0]).Text;
+            string zipcode = ((TextBox)row.Cells[6].Controls[0]).Text;
+            string phoneno = ((TextBox)row.Cells[7].Controls[0]).Text;
+            string comment = ((TextBox)row.Cells[8].Controls[0]).Text;
+            string totalamt = ((TextBox)row.Cells[10].Controls[0]).Text;
+            string paymentdate = ((TextBox)row.Cells[11].Controls[0]).Text;
+            string deliverystatus = ((TextBox)row.Cells[12].Controls[0]).Text;
+
+
+
+            result = order.checkoutUpdate(int.Parse(order_ID), fname, lname, country, city, address, zipcode,
+                            phoneno, comment, decimal.Parse(totalamt),paymentdate, deliverystatus);
+            if (result > 0)
+            {
+                Response.Write("<script>alert('Orders updated successfully');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Orders NOT updated');</script>");
+            }
+            Gv_Orders.EditIndex = -1;
+            bind();
+        }
+
+        protected void btn_submitUp_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            Checkout order = new Checkout();
+            int id = int.Parse(lbl_orderid.Text);
+            string fname = lbl_fname.Text;
+            string lname = lbl_lname.Text;
+            string country = lbl_country.Text;
+            string city = lbl_city.Text;
+            string address = lbl_address.Text;
+            string zipcode = lbl_zipcode.Text;
+            string hp = lbl_phoneno.Text;
+            string comments = lbl_comment.Text;
+            decimal totalamt = decimal.Parse(lbl_totalamt.Text);
+            string date = lbl_date.Text;
+            string status = rbl_status.SelectedValue;
+
+
+
+
+
+
+            result = order.checkoutUpdate(id, fname, lname, country, city, address, zipcode, hp, comments, totalamt, date, status);
+            if (result > 0)
+            {
+                Response.Write("<script>alert(' Status updated successfully');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Status NOT updated successfully');</script>");
+            }
+            bind();
+        }
+
+        protected void Gv_Orders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = Gv_Orders.SelectedRow;
+
+            string orderID = row.Cells[0].Text;
+            Checkout aOrders = new Checkout();
+            order = aOrders.getOrder(orderID);
+
+            lbl_orderid.Text = order.Order_ID.ToString();
+            lbl_fname.Text = order.FName;
+            lbl_lname.Text = order.LName;
+            lbl_country.Text = order.Country;
+            lbl_city.Text = order.City;
+            lbl_address.Text = order.Address;
+            lbl_zipcode.Text = order.Zipcode;
+            lbl_phoneno.Text = order.PhoneNo;
+            lbl_comment.Text = order.Comment;
+            lbl_products.Text = order.Product;
+            lbl_totalamt.Text = order.TotalAmount.ToString();
+            lbl_date.Text = order.PaymentDate;
+            rbl_status.Text = order.DeliveryStatus;
+
+
+            if (order.DeliveryStatus == "Undelivered")
+            {
+                rbl_status.SelectedValue = "Undelivered";
+                
+            }
+            else if (order.DeliveryStatus == "In Transit")
+            {
+                rbl_status.SelectedValue = "In Transit";
+            }
+            else if (order.DeliveryStatus == "Delivered")
+            {
+
+                rbl_status.SelectedValue = "Delivered";
+                rbl_status.Enabled = false;
+            }
+            else
+            {
+                rbl_status.SelectedValue = "Awaiting Design";
+
+            }
+
+
+
+
+        }
+
+
     }
 }
