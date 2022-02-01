144dc9c9fb56f3153a6ead1aa31c5d32a6c75f06
explodingblu <190255S@mymail.nyp.edu.sg>
2022-02-01 11:32:45 +0800

    Issues

--- BIPJ-Grp2-Team5/Customer_AddReview.aspx.cs
+++ BIPJ-Grp2-Team5/Customer_AddReview.aspx.cs
@@ -9,81 +9,9 @@ namespace BIPJ_Grp2_Team5
 {
     public partial class Customer_AddReview : System.Web.UI.Page
     {
-        Product aProd = new Product();
-        List<Product> prodList = new List<Product>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            // Load sample data only once, when the page is first loaded.
-            if (!Page.IsPostBack)
-            {
-                DDLRateBindData();
-                ImgBtnBindData();
-            }
-        }
-        protected void DDLRateBindData()
-        {
-            prodList = aProd.getProductAll();// returns a list full of class
 
-            DDL_Product.DataSource = prodList;
-            DDL_Product.DataTextField = "Product_Name";
-            DDL_Product.DataValueField = "Product_Name";
-            DDL_Product.DataBind();
-
-        }
-
-        protected void ImgBtnBindData()
-        {
-            ImgBtn_Rating1.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating2.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating3.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating4.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating5.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-
-        }
-
-        protected void ImgBtn_Rating1_Click(object sender, ImageClickEventArgs e)
-        {
-            ImgBtn_Rating1.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating2.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating3.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating4.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating5.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-        }
-
-        protected void ImgBtn_Rating2_Click(object sender, ImageClickEventArgs e)
-        {
-            ImgBtn_Rating1.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating2.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating3.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating4.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating5.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-        }
-
-        protected void ImgBtn_Rating3_Click(object sender, ImageClickEventArgs e)
-        {
-            ImgBtn_Rating1.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating2.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating3.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating4.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-            ImgBtn_Rating5.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-        }
-
-        protected void ImgBtn_Rating4_Click(object sender, ImageClickEventArgs e)
-        {
-            ImgBtn_Rating1.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating2.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating3.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating4.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating5.ImageUrl = "~/Images/EmptyStarRating.jpeg";
-        }
-
-        protected void ImgBtn_Rating5_Click(object sender, ImageClickEventArgs e)
-        {
-            ImgBtn_Rating1.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating2.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating3.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating4.ImageUrl = "~/Images/FullStarRating.jpeg";
-            ImgBtn_Rating5.ImageUrl = "~/Images/FullStarRating.jpeg";
         }
     }
 }
